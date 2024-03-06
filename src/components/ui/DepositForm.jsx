68538eb46daa85664ed4c6d5e05c13f10e69914c
krypt007 <steve@calstec.com>
2024-03-06 09:27:38 -0500

    added deposit form validation

--- src/components/ui/DepositForm.jsx
+++ src/components/ui/DepositForm.jsx
@@ -20,6 +20,8 @@ import {
   CreditCardIcon,
   LockClosedIcon,
 } from "@heroicons/react/24/solid";
+
+import { useForm } from 'react-hook-form';
  
 function formatCardNumber(value) {
   const val = value.replace(/\s+/g, "").replace(/[^0-9]/gi, "");
@@ -48,30 +50,61 @@ function formatExpires(value) {
 }
  
 export default function DepositForm() {
+  const { register, formState: { errors }, handleSubmit, reset, setValue } = useForm();
+
   const { countries } = useCountries();
   const [type, setType] = useState("card");
   const [cardNumber, setCardNumber] = useState("");
   const [cardExpires, setCardExpires] = useState("");
-  const [payStatus, setPayStatus] = useState(2);
+  const [payStatus, setPayStatus] = useState(1);
+
+  function _handleSubmit (e) {
+    setPayStatus(2); // Set the payment status to processing
+
+    // Simulate an API call or any asynchronous operation
+    setTimeout(() => {
+      // Once the operation is complete, set the payment status accordingly
+      setPayStatus(3); // Payment successful
+    }, 2000); // Simulated delay of 2 seconds
+
+  }
+
+  const onSubmit = (data) => {
+    setValue("cardNumber", cardNumber)
+    alert(JSON.stringify(data));
+    fetch('/api/registerNewOffender',{
+       method: 'POST',
+       body: JSON.stringify(data),
+    }).then(()=>{
+       console.log('data')
+      //  refreshOffendersList()
+    }).catch(error => {
+      console.log('error', error);
+    });
+    // setOffenders((offenders) => [...offenders, ...data]);
+  
+    reset();
+    setModal(!modal);
+  };
  
   return (
-    <div className="flex justify-center w-full align-center h-[90%] border-black p-2">
-        <Card className="w-full max-w-[24rem] bg-blue-400">
+    <div className="flex justify-center w-full align-center h-[90%] border-black p-0">
+        <Card className="w-full max-w-[24rem] bg-blue-400 rounded-sm">
             <CardHeader
                 color="white"
                 floated={false}
                 shadow={false}
-                className="m-0 grid place-items-center px-4 py-8 text-center"
+                className="m-0 grid place-items-center px-4 py-2 text-center"
             >
-                <div className="mb-4 h-20 p-6 text-white">
+                <div className="mb-4 h-10 p-2 text-white">
                 {type === "card" ? (
                     <CreditCardIcon className="h-10 w-10 text-blue" />
                 ) : (
                     <img alt="paypal " className="w-14 " src="https://docs.material-tailwind.com/icons/paypall.png" />
                 )}
                 </div>
-                <Typography variant="h5" color="blue">
-                Cybersource Make Payment
+                <Typography variant="h3" color="blue">
+                  Cybersource Make Payment
                 </Typography>
             </CardHeader>
             <CardBody>
@@ -99,104 +132,113 @@ export default function DepositForm() {
                     }}
                 >
                     <TabPanel value="card" className="p-0">
-                    <form className="mt-12 flex flex-col gap-4">
+                    <form className="mt-6 flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
                         <div>
-                        <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="mb-2 font-medium"
-                        >
-                            Your Email
-                        </Typography>
-                        <Input
-                            type="email"
-                            placeholder="name@mail.com"
-                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                            labelProps={{
-                            className: "before:content-none after:content-none",
-                            }}
-                        />
+                            <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
+                                <label className="block mb-1 text-sm text-left font-medium text-white" htmlFor="email">Your Email:</label>
+                            </Typography>
+                            <input className="col-span-3 bg-blue-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="name@mail.com"
+                                {...register('email', {
+                                    required: 'email is required',
+                                    pattern: {
+                                        value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                        message: 'Please enter a valid email',
+                                    },
+                                })}
+                            />
+                            <p className="block col-span-4 text-xs text-violet-800">{errors.email && errors.email.message}</p>
                         </div>
         
-                        <div className="my-3">
-                        <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="mb-2 font-medium "
-                        >
-                            Card Details
-                        </Typography>
-        
-                        <Input
-                            maxLength={19}
-                            value={formatCardNumber(cardNumber)}
-                            onChange={(event) => setCardNumber(event.target.value)}
-                            icon={
-                            <CreditCardIcon className="absolute left-0 h-4 w-4 text-blue-gray-300" />
-                            }
-                            placeholder="0000 0000 0000 0000"
-                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                            labelProps={{
-                            className: "before:content-none after:content-none",
-                            }}
-                        />
-                        <div className="my-4 flex items-center gap-4">
-                            <div>
-                            <Typography
-                                variant="small"
-                                color="blue-gray"
-                                className="mb-2 font-medium"
-                            >
-                                Expires
+                        <div className="my-1">
+                        <div>
+                            <Typography variant="small" color="blue-gray" className="mb-2 font-medium ">
+                            <label className="block mb-1 text-sm text-left font-medium text-white" htmlFor="cardNumber">Card Details:</label>
                             </Typography>
-                            <Input
-                                maxLength={5}
-                                value={formatExpires(cardExpires)}
-                                onChange={(event) => setCardExpires(event.target.value)}
-                                containerProps={{ className: "min-w-[72px]" }}
+
+                            <input className="col-span-3 bg-blue-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="0000 0000 0000 0000"
+                                // value={formatCardNumber(cardNumber)}
+                                // onChange={(event) => setCardNumber(event.target.value)}
+                                {...register('cardNumber', {
+                                    required: 'Card No is required',
+                                    minLength: {
+                                        value: 16,
+                                        message: 'Min length is 16',
+                                    },
+                                    maxLength: {
+                                        value: 19,
+                                        message: 'Max length is 19',
+                                    },
+                                    pattern: {
+                                        value: /^\d{4}(?:\s\d{4}){3}$/,
+                                        message: 'Please enter a valid card number',
+                                    },
+                                })}
+                            />
+                            <p className="block col-span-4 text-xs text-violet-800">{errors.cardNumber && errors.cardNumber.message}</p>
+                        </div>
+
+                        <div className="my-4 flex items-center gap-4">                        
+                        <div>
+                            <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
+                                <label className="block mb-1 text-sm text-left font-medium text-white" htmlFor="cardExpires">Expires:</label>
+                            </Typography>
+                            <input className="col-span-3 bg-blue-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 focus:!border-t-gray-900 block min-w-[72px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="00/00"
-                                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                                labelProps={{
-                                className: "before:content-none after:content-none",
-                                }}
+                                {...register('cardExpires', {
+                                    required: 'Card Expiry required',
+                                    minLength: {
+                                        value: 5,
+                                        message: 'Min length is 5',
+                                    },
+                                    maxLength: {
+                                        value: 5,
+                                        message: 'Max length is 5',
+                                    },
+                                })}
                             />
-                            </div>
-                            <div>
-                            <Typography
-                                variant="small"
-                                color="blue-gray"
-                                className="mb-2 font-medium"
-                            >
-                                CVC
-                            </Typography>
-                            <Input
-                                maxLength={4}
-                                containerProps={{ className: "min-w-[72px]" }}
-                                placeholder="000"
-                                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                                labelProps={{
-                                className: "before:content-none after:content-none",
-                                }}
-                            />
-                            </div>
+                            <p className="block col-span-4 text-xs text-violet-800">{errors.cardExpires && errors.cardExpires.message}</p>
                         </div>
-                        <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="mb-2 font-medium"
-                        >
-                            Holder Name
+                        <div>
+                        <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
+                            <label className="block mb-1 text-sm text-left font-medium text-white" htmlFor="cardCvc">CVC:</label>
                         </Typography>
-                        <Input
+                        <input className="col-span-3 bg-blue-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 focus:!border-t-gray-900 block min-w-[72px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="000"
+                                {...register('cardCvc', {
+                                    required: 'CVC required',
+                                    minLength: {
+                                        value: 3,
+                                        message: 'Min length is 3',
+                                    },
+                                    maxLength: {
+                                        value: 4,
+                                        message: 'Max length is 4',
+                                    },
+                                })}
+                            />
+                            <p className="block col-span-4 text-xs text-violet-800">{errors.cardCvc && errors.cardCvc.message}</p>
+                        </div>
+                        </div>
+                        <Typography variant="small" color="blue-gray" className="mb-2 font-medium">
+                            <label className="block mb-1 text-sm text-left font-medium text-white" htmlFor="cardHolderName">Holder Name:</label>
+                        </Typography>
+                        <input className="col-span-3 bg-blue-200 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="John Smith"
-                            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                            labelProps={{
-                            className: "before:content-none after:content-none",
-                            }}
+                            {...register('cardHolderName', {
+                                required: 'Full name is required',
+                                pattern: {
+                                    value: /^[A-Z][a-z]+ [A-Z][a-z]+$/,
+                                    message: 'Full name is required',
+                                }
+                            })}
                         />
+                        <p className="block col-span-4 text-xs text-violet-800">{errors.cardHolderName && errors.cardHolderName.message}</p>
                         </div>
                         
-                            { (payStatus == 1) ? (<Button size="lg">Pay Now</Button>): (payStatus == 2) ? (<Button size="lg" className="bg-blue-300">Processing</Button>): (payStatus == 3) ? (<Button size="lg">Payment Successful</Button>): (<Button size="lg">Error</Button>)}
+                            {/* { (payStatus == 1) ? (<Button size="lg" type="submit" className="bg-blue-300" onClick={handleSubmit}>Pay Now</Button>): (payStatus == 2) ? (<Button size="lg">Processing</Button>): (payStatus == 3) ? (<Button size="lg">Payment Successful</Button>): (<Button size="lg">Error</Button>)} */}
+                        <button type="submit" className="w-full text-white bg-[#101A28] hover:bg-[#1A2C38] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Pay Now</button>
                         
                         <Typography
                         variant="small"
