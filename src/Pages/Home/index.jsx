35d4f20635a80ddb29a994cb7f685ada8ec9f9db
krypt007 <steve@calstec.com>
2024-03-16 14:04:21 -0400

    Home page refactor

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="bg-primary w-screen overflow-hidden">
         <div className={`flex flex-col bg-black flex justify-center items-center`}>
           <div className='flex h-[5%] p-2 bg-gray-800 w-full justify-between '>
-            <div>Navbar</div>
+            <div className='p-2'><div className='p-2 rounded-full w-[24px] h-[24px] bg-gradient-to-br from-red-500 to-pink-200 animate-pulse' /></div>
             <div className='flex gap-2 text-sm font-bold'>
               <div className='hover:cursor-pointer hover:text-red-300'>
                 <Link to="/Deposit" className='text-white hover:text-red-300'> <div className='p-2 rounded-md hover:bg-gray-400'>Donate</div></Link>
